121c783718984592f479c27204e95fd5a6af6888
nickhill2380 <nickhill2380@gmail.com>
2021-01-29 17:41:03 -0600

    add tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,10 +55,8 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
   Category.update({
-    
       category_name: req.body.category_name,
       where: {id: req.params.id},
-    
     }
     ).then(response => {
       if(!response) {
